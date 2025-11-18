0c9b30b695b9f7a8b6c204d9d7c644656828b448
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-11-18 18:17:08 +0000

    update

--- commands_&_sample_programs/bash-arithmetic.sh
+++ commands_&_sample_programs/bash-arithmetic.sh
@@ -2,12 +2,15 @@
 # arithmetic : +,-,*,/,%
 #syntax : $((arithmetic))
 
-echo $((5+2))
-echo $((5-2))
-echo $((5*2))
-echo $((5/2))
+echo $((2+2))
+echo $((7-3))
+echo $((4*2))
+echo $((9/3))
 echo $((5%2))
 
 a=7
 b=3
 echo $((a+b))
+
+
++-/*
