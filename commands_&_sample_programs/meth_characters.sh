14d1c9ca91a4c0cae3e90929acc3a9917d8b49aa
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-11-19 03:15:04 +0000

    meth_characters

---
+++ commands_&_sample_programs/meth_characters.sh
@@ -0,0 +1,23 @@
+# Author : ataulmohsin
+# echo "comment"
+# '#' for comment
+
+
+# ";" for command seperator
+echo "Hello"; echo "World"
+
+
+# "\" mutiple lines script
+echo \
+"Hello" \
+"World"
+
+
+# "*" for string expansion wildcard
+ls /bin/ba* # list all files or folders which start from 'ba'
+ls *.txt
+
+
+# "?" for single character expansion wildcard
+ls /bin/?ash    # list all files or folders which has 4 char name and ending with 'ash'
+ls file2013-02-??.txt 
