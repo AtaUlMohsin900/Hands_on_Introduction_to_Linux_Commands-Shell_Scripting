5ab663f22cb400f8a2944b126dd6be08f6afc54b
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-11-19 03:22:21 +0000

    pipe_prarallel_seq

---
+++ commands_&_sample_programs/pipe-prarallel-sequence.sh
@@ -0,0 +1,22 @@
+# Author : ataulmohsin
+
+# run command piping  [command] | [command] | ......
+sort alphabit.txt | uniq 
+sort alphabit.txt | uniq | tr "[a-z]" "[A-Z]" # tr for replace characters.
+
+# multiple line piping
+cat alphabit.txt |\
+uniq |\
+tr "[a-z]" "[A-Z]" # tr for replace characters.
+
+
+# Run each command Sequencially
+echo "Sequencial :"
+echo "AA" ; echo "BB" ; echo "CC" ; echo "DD" ; 
+echo "EE"
+
+# Run each command parallely
+echo "Parallel :"
+echo "A" & echo "B" & echo "C" &\
+ echo "D" &\
+  echo "E"
