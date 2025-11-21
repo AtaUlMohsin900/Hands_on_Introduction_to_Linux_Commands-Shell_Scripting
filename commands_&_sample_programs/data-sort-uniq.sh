49db1439fc5bb798ec988d0b50986e2b89b01316
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-11-21 18:52:19 +0000

    data-sorting

---
+++ commands_&_sample_programs/data-sort-uniq.sh
@@ -0,0 +1,10 @@
+# Author : ataulmohsin
+# sort for sort data
+echo "Sort Forward: "
+sort age.txt
+echo "Sort Reverse: "
+sort -r age.txt
+
+# filter repeated data
+echo "Unique: "
+uniq animal.txt
