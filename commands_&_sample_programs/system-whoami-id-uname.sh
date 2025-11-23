e32a6129bf4a0529e3801fd0f7f463aefee4eb7d
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-11-23 12:25:38 +0000

    system-whoami

---
+++ commands_&_sample_programs/system-whoami-id-uname.sh
@@ -0,0 +1,19 @@
+# whoami to get username
+whoami
+username=`whoami`
+echo $username
+
+# id to get user id or group id
+id
+id -u
+id -u -n
+ii=`id`
+echo $ii
+
+# uname to get OS name version
+uname
+uname -s -r
+uname -v
+uname -a
+un=`uname`
+echo $un
